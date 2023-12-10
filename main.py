532206e6dd1e4dd973525140021dfd75707a8bdb
Marton János <martoncisco@gmail.com>
2023-12-10 16:52:59 +0100

    testfeladat

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ class Bicikli(ABC):
     def display_info(self):
         pass
 
-
 class OrszagutiBicikli(Bicikli):
     def __init__(self, tipus, ar, allapot):
         super().__init__(tipus, ar, allapot)
@@ -19,15 +18,12 @@ class OrszagutiBicikli(Bicikli):
     def display_info(self):
         print(f"Országúti bicikli - Tipus: {self.tipus}, Ár: {self.ar}, Állapot: {self.allapot}")
 
-
 class HegyiBicikli(Bicikli):
     def __init__(self, tipus, ar, allapot):
         super().__init__(tipus, ar, allapot)
 
-
     def display_info(self):
         print(f"Hegyi bicikli - Tipus: {self.tipus}, Ár: {self.ar}, Állapot: {self.allapot}")
-
 
 class Kolcsonzo:
     def __init__(self, nev):
@@ -44,12 +40,27 @@ class Kolcsonzo:
             bicikli.display_info()
             print("---------")
 
+class Kolcsonzes:
+    def __init__(self, kolcsonzo, bicikli, idopont):
+        self.kolcsonzo = kolcsonzo
+        self.bicikli = bicikli
+        self.idopont = idopont
 
+    def display_info(self):
+        print(f"Kölcsönző: {self.kolcsonzo.nev}")
+        print(f"Bicikli:")
+        self.bicikli.display_info()
+        print(f"Kölcsönzés időpontja: {self.idopont}")
+
+# Példa használatra
 orszaguti_bicikli = OrszagutiBicikli(tipus="Országúti", ar=800, allapot="Új")
 hegyi_bicikli = HegyiBicikli(tipus="Hegyi", ar=1000, allapot="Használt")
 
-kolcsonzo = Kolcsonzo(nev="Kerékpárkölcsönző kft.")
+kolcsonzo = Kolcsonzo(nev="Kerékpár kft.")
 kolcsonzo.add_bicikli(orszaguti_bicikli)
 kolcsonzo.add_bicikli(hegyi_bicikli)
 
 kolcsonzo.display_info()
+
+kolcsonzes = Kolcsonzes(kolcsonzo=kolcsonzo, bicikli=orszaguti_bicikli, idopont="2023-12-10 14:30")
+kolcsonzes.display_info()
